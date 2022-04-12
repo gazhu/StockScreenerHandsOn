fa5d4d291621869701b662e923aa7fabc6d3358b
ManishSehrawat <manishsehrawat9896@gmail.com>
2022-04-12 08:01:10 +0530

    Comment Updations

--- src/test/java/com/ey/wamacademy/capstoneapi/controller/MainControllerTest.java
+++ src/test/java/com/ey/wamacademy/capstoneapi/controller/MainControllerTest.java
@@ -26,18 +26,20 @@ class MainControllerTest {
 	@SuppressWarnings("serial")
 	List<LandingPage> records = new ArrayList<LandingPage>() {
 		{
-			add(new LandingPage(1,"30 March 2022", "/BAM", "Brookfield Asset Management Ord Shs Class A", "USD",
+			add(new LandingPage(1, "30 March 2022", "/BAM", "Brookfield Asset Management Ord Shs Class A", "USD",
 					"CA1125851040", "2092555", "BAM", 57.86, 57.61, 58.35, 57.38, 859012, 3.857941229, 5.435578331,
 					31.64220584, 94368982323L, 1.294516689, 16023304.64, 2.3963, 23.87484459,
 					"United States of America", "NYSE", "Financials", "Capital Markets", "Ordinary Shares"));
-			add(new LandingPage(2,"31 March 2022", "/CAN", "Brookfield Asset Management Ord Shs Class A", "USD",
+			add(new LandingPage(2, "31 March 2022", "/CAN", "Brookfield Asset Management Ord Shs Class A", "USD",
 					"CA1125851040", "2092666", "BAM", 57.86, 57.61, 58.35, 57.38, 859012, 3.857941229, 5.435578331,
 					31.64220584, 94368982323L, 1.294516689, 16023304.64, 2.3963, 23.87484459,
 					"United States of America", "NYSE", "Financials", "Capital Markets", "Ordinary Shares"));
 		}
 	};
 
-	// Testing viewAll by verifying the number of records fetched
+	/**
+	 * Testing viewAll by verifying the number of records fetched
+	 */
 	@Test
 	void viewAllTest() {
 
--- src/test/java/com/ey/wamacademy/capstoneapi/dao/LandingPageDaoTests.java
+++ src/test/java/com/ey/wamacademy/capstoneapi/dao/LandingPageDaoTests.java
@@ -20,11 +20,11 @@ class LandingPageDaoTests {
 	@SuppressWarnings("serial")
 	List<LandingPage> records = new ArrayList<LandingPage>() {
 		{
-			add(new LandingPage(1,"30 March 2022", "/BAM", "Brookfield Asset Management Ord Shs Class A", "USD",
+			add(new LandingPage(1, "30 March 2022", "/BAM", "Brookfield Asset Management Ord Shs Class A", "USD",
 					"CA1125851040", "2092555", "BAM", 57.86, 57.61, 58.35, 57.38, 859012, 3.857941229, 5.435578331,
 					31.64220584, 94368982323L, 1.294516689, 16023304.64, 2.3963, 23.87484459,
 					"United States of America", "NYSE", "Financials", "Capital Markets", "Ordinary Shares"));
-			add(new LandingPage(2,"31 March 2022", "/CAN", "Brookfield Asset Management Ord Shs Class A", "USD",
+			add(new LandingPage(2, "31 March 2022", "/CAN", "Brookfield Asset Management Ord Shs Class A", "USD",
 					"CA1125851040", "2092666", "BAM", 57.86, 57.61, 58.35, 57.38, 859012, 3.857941229, 5.435578331,
 					31.64220584, 94368982323L, 1.294516689, 16023304.64, 2.3963, 23.87484459,
 					"United States of America", "NYSE", "Financials", "Capital Markets", "Ordinary Shares"));
@@ -38,27 +38,31 @@ class LandingPageDaoTests {
 	@MockBean
 	private LandingPageDao landingPageDao;
 
-	// Method for testing that empty result is not returned by viewAll method
+	/**
+	 * Method for testing that empty result is not returned by viewAll method
+	 */
 	@Test
 	void viewAllTest() {
 		when(landingPageDao.viewAll()).thenReturn(records);
 		assertThat(landingPageService.fetchAllData().size()).isGreaterThan(0);
 	}
 
-	// Method for verifying column count for fetched data
+	/**
+	 * Method for verifying column count for fetched data
+	 */
 	@Test
 	void columnCountTest() {
 		when(landingPageDao.getColumnCount()).thenReturn(25);
 		assertEquals(25, landingPageDao.getColumnCount());
 	}
 
-	/*
+	/**
 	 * Method for verifying right record is returned when searching by ISIN using
 	 * searchByID method
 	 */
 	@Test
 	void searchByIdTest() {
-		LandingPage actualValue = new LandingPage(1,"30 March 2022", "/BAM",
+		LandingPage actualValue = new LandingPage(1, "30 March 2022", "/BAM",
 				"Brookfield Asset Management Ord Shs Class A", "USD", "CA1125851040", "2092555", "BAM", 57.86, 57.61,
 				58.35, 57.38, 859012, 3.857941229, 5.435578331, 31.64220584, 94368982323L, 1.294516689, 16023304.64,
 				2.3963, 23.87484459, "United States of America", "NYSE", "Financials", "Capital Markets",
@@ -68,7 +72,9 @@ class LandingPageDaoTests {
 		assertThat(actualValue).usingRecursiveComparison().isEqualTo(expectedValue);
 	}
 
-	// Method for verifying number of records fetched when viewAll method is called
+	/**
+	 * Method for verifying number of records fetched when viewAll method is called
+	 */
 	@Test
 	void viewAllTestCount() {
 
